ae48e43a5b08fce8ec3ec3a09a923526ab25e657
Molikuc <ruiz.alexis1996@gmail.com>
2021-11-18 15:11:49 +0100

    new files, you can now change the size of the grid

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const container = document.querySelector('#container');
-
+let start = 16;
+container.style.setProperty('--grid-cols', start);
 
 
 /* Create a grid with column and row and create a property for getting them in a grid in the css files */
 
-function grid(rows, cols){
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for(let i=0; i<(rows * cols); i++){
+function grid(){
+    container.innerHTML = "";
+    for(let i=0; i<(start * start); i++){
         let cell = document.createElement('div');
         container.appendChild(cell).className = 'cell';
         cell.addEventListener('mouseover', () =>{
@@ -16,4 +16,15 @@ function grid(rows, cols){
     }   
 }
 
-grid(16,16);
+function newGrid(){
+    start = Math.round(parseInt(prompt('Please enter your new grid size (below 100 squares)')));
+    if (start < 100){
+        container.style.setProperty('--grid-cols', start);
+        grid(start, start);
+    } else {
+        alert('Above 100 squares');
+    }
+    
+}
+
+grid();
